d2c49327df9f5c431b355dc4779504a1f9c02da3
Saheen Alam Shuvo <saheenshuvo182@gmail.com>
2026-03-17 00:38:56 +0600

    Delete One

---
+++ delete-$deleteOne.ts
@@ -0,0 +1,12 @@
+{
+    // $deleteOne
+
+    //db.test.deleteOne(
+    //  {
+    //    _id: ObjectId("69a813e5b474b36529b57bfa"),
+    //  },
+    //);
+
+    // Output:
+    // The deleteOne method is used to delete a single document from a collection that matches the specified filter. In this case, the query will find the document with the specified _id and delete it from the test collection. If a document with the specified _id exists, it will be removed from the collection. If no document matches the filter, no changes will be made to the collection. The deleteOne method will only delete one document, even if multiple documents match the filter criteria.
+}
