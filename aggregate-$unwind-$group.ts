a7da4811ecc14efd4e71a668bae0a325526bdd84
Saheen Alam Shuvo <saheenshuvo182@gmail.com>
2026-05-07 01:18:07 +0600

    and  with aggregate

---
+++ aggregate-$unwind-$group.ts
@@ -0,0 +1,15 @@
+{
+    //db.test.aggregate([
+    //stage-1
+    // {
+    //     $unwind: "$interests"
+    // },
+    //stage-2
+    // {
+    //     $group: {
+    //         _id: "$age",
+    //         interestsPerAge: { $push: "$interests" }
+    //     }
+    // }
+    //])
+}
