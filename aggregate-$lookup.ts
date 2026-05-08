4b24d8969670ce41c8109be27d50196998263309
Saheen Alam Shuvo <saheenshuvo182@gmail.com>
2026-05-08 19:38:31 +0600

    for referencing

---
+++ aggregate-$lookup.ts
@@ -0,0 +1,12 @@
+{
+    // db.test.aggregate([
+        // {
+        //     $lookup: {
+        //         from: "orders", // The collection to join with
+        //         localField: "customerId", // The field from the input documents
+        //         foreignField: "customerId", // The field from the documents of the "from" collection
+        //         as: "ordersInfo" // The name of the new array field to add to the input documents
+        //     }
+        // }
+    // ])
+}
